58fcbc6598013d23defe298b1d9baa7827971158
Siegfriedj2018 <siegfrij@email.sc.edu>
2025-10-01 02:58:32 -0400

    added equals to base layer

--- xdmDK/keymap.c
+++ xdmDK/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,                                         KC_RBRC,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_GRAVE,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           LSFT(KC_LBRC),                             LSFT(KC_RBRC),          KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_ENTER,       
     KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                                              KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT, 
-    KC_LEFT_CTRL,   KC_CAPS,        KC_CYCLE_LAYERS,KC_LEFT,       KC_RIGHT,       KC_LEFT_GUI,                                                                 DUAL_FUNC_0,           KC_UP,          KC_DOWN,        KC_NO,          KC_NO,          KC_LEFT_CTRL,   
+    KC_LEFT_CTRL,   KC_CAPS,        KC_CYCLE_LAYERS,KC_LEFT,       KC_RIGHT,       KC_LEFT_GUI,                                                                 DUAL_FUNC_0,           KC_UP,          KC_DOWN,        KC_EQUAL,       KC_NO,          KC_LEFT_CTRL,   
     KC_SPACE,       KC_BSPC,        KC_LEFT_ALT,                    KC_ENTER,       KC_TAB,         KC_SPACE
   ),
   [1] = LAYOUT_moonlander(
